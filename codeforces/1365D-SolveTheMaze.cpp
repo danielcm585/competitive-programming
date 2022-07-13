9b10b98ce863cedd5569f5ff1c0b3f97012b9137
danielcm585 <danielcm585@gmail.com>
2022-07-13 14:30:29 +0700

    solve cf 1365D

--- codeforces/1365D-SolveTheMaze.cpp
+++ codeforces/1365D-SolveTheMaze.cpp
@@ -24,7 +24,7 @@ int main() {
   int tc; cin >> tc;
   while (tc--) {
     cin >> n >> m;
-    a = vector<vector<char> >(n+2,vector<char>(m+2));
+    a = vector<vector<char> >(n+2, vector<char>(m+2));
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= m; j++) {
         cin >> a[i][j];
@@ -34,33 +34,35 @@ int main() {
       for (int j = 1; j <= m; j++) {
         if (a[i][j] != 'B') continue;
         for (int k = 0; k < 4; k++) {
-          int r = i + dr[k];
-          int c = j + dc[k];
+          int r = i+dr[k], c = j+dc[k];
           if (valid(r,c) && a[r][c] == '.') a[r][c] = '#';
         }
       }
     }
-    vis = vector<vector<bool> >(n+2,vector<bool>(m+2));
-    queue<ii> q; q.push(ii(n,m)); vis[n][m] = 1;
+    vis = vector<vector<bool> >(n+2, vector<bool>(m+2));
+    queue<ii> q; q.push(ii(n,m)); 
+    vis[n][m] = 1;
     while (!q.empty()) {
       ii cur = q.front(); q.pop();
       int r = cur.fi, c = cur.se;
       if (a[r][c] == '#') continue;
       for (int i = 0; i < 4; i++) {
-        int nr = r+dr[i];
-        int nc = c+dc[i];
-        if (valid(nr,nc) && a[r][c] != '#') {
+        int nr = r+dr[i], nc = c+dc[i];
+        if (!valid(nr,nc) || vis[nr][nc]) continue;
+        if (a[nr][nc] != '#') {
           vis[nr][nc] = 1;
           q.push(ii(nr,nc));
         }
       }
     }
     bool good = 1;
-    for (int i = 1; i <= n && good; i++) {
-      for (int j = 1; j <= m && good; j++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        // cout << vis[i][j];
         if (a[i][j] == 'G' && !vis[i][j]) good = 0;
         if (a[i][j] == 'B' && vis[i][j]) good = 0;
       }
+      // cout << '\n';
     }
     cout << (good ? "Yes" : "No") << '\n';
   }
